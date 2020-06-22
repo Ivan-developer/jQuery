abc7be5d44021a06e6aebbca51ca9384468a4950
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-06-22 17:30:54 +0300

    add

--- UNIT 8. Calculator/index.js
+++ UNIT 8. Calculator/index.js
@@ -1,6 +1,32 @@
 $(function(){
    $('#formSum').on('submit', function(event){
        event.preventDefault();
+
+       var numberOne,
+       numberTwo,
+       sumn;
+
+       numberOne = $('#numberOne').val(); // из input выходит string
+       numberTwo = $('#numberTwo').val();
+
+       // метод isNaN = является ли число или нет
+
+       if( isNaN(numberOne)){
+           numberOne = 0;
+       }
+
+       if ( isNaN(numberTwo)){
+           numberTwo = 0;
+       }
+
+    //    sumn = Number(numberOne) +  Number(numberTwo);
+
+    // Number() === parseInt() - превращает строку в число
+
+    sumn = parseInt(numberOne) +  parseInt(numberTwo);
+
+    $('#sunResult').text(`Сумма: ${sumn}`)
+       
    })
 })
 
